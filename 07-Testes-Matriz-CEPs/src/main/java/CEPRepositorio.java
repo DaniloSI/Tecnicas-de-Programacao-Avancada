4fc0529daabbd6fcd8622fd4e69c7b88731c7bc3
Danilo de Oliveira <danilo.psn94@gmail.com>
2017-04-12 23:41:48 -0300

    CEP adicionado

--- 07-Testes-Matriz-CEPs/src/main/java/CEPRepositorio.java
+++ 07-Testes-Matriz-CEPs/src/main/java/CEPRepositorio.java
@@ -20,12 +20,13 @@ public class CEPRepositorio {
         while ((line = bufferedReader.readLine()) != null) {
             String[] fields = line.split(";");
             CEP newCEP = new CEP();
+            String regexRemoveWhiteSpaces = "(\\s+$)|(^\\s+)";
 
-            newCEP.setCep(fields[0].replaceAll("\\s+", ""));
-            newCEP.setRua(fields[1].replaceAll("(\\s+$)|(^\\s+)", ""));
-            newCEP.setBairro(fields[2].replaceAll("(\\s+$)|(^\\s+)", ""));
-            newCEP.setMunicipio(fields[3].split(",")[0].replaceAll("(\\s+$)|(^\\s+)", ""));
-            newCEP.setEstado(fields[3].split(",")[1].replaceAll("(\\s+$)|(^\\s+)", ""));
+            newCEP.setCep(fields[0].replaceAll(regexRemoveWhiteSpaces, ""));
+            newCEP.setRua(fields[1].replaceAll(regexRemoveWhiteSpaces, ""));
+            newCEP.setBairro(fields[2].replaceAll(regexRemoveWhiteSpaces, ""));
+            newCEP.setMunicipio(fields[3].split(",")[0].replaceAll(regexRemoveWhiteSpaces, ""));
+            newCEP.setEstado(fields[3].split(",")[1].replaceAll(regexRemoveWhiteSpaces, ""));
 
             ceps.insert(new ItemTabelaHash<>(newCEP.getCep(), newCEP));
         }
